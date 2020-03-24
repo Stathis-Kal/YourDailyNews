97db5489d7229ce4c9414fe567db322f039b211d
Stathis-Kal <sane31stathis@hotmail.com>
2020-03-24 12:55:44 +0200

    added the toggle dark theme functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ThemeService } from './shared/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit{
+   isDarkTheme: Observable<boolean>;
+   
+   constructor(private themeService: ThemeService){}
+
+   ngOnInit(){
+     this.isDarkTheme = this.themeService.isDarkTheme;
+   }
 }
